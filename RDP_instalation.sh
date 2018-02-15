0c8caa9c87e52fc90ea3ff134d28ea9f4e17ae8d
Farkhod <sadykovfarkhod@gmail.com>
2018-02-14 20:08:55 -0600

    Instalation RDP for Centos 7
    Remote Desktop Conection Centos 7>/dev/null

---
+++ RDP_instalation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+functiondownload(){
+	echo -e "[xrdp]
+name=xrdp
+baseurl=http://li.nux.ro/download/nux/dextop/el7/x86_64/
+enabled=1
+gpgcheck=0" > /etc/yum.repos.d/xrdp.repo
+	rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm >/dev/null
+	rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm >/dev/null
+	yum -y install xrdp tigervnc-server
+	systemctl enable xrdp.service 
+	firewall-cmd --permanent --zone=public --add-port=3389/tcp
+	firewall-cmd --reload
+	chcon --type=bin_t /usr/sbin/xrdp
+	chcon --type=bin_t /usr/sbin/xrdp-sesman
+	systemctl start xrdp.service
+
+} 
+
+echo "${green}###############################################################${reset}"
+echo "${green}################ This is RDP instalation ######################${reset}"
+echo "${green}###############################################################${reset}"
+read -p "${green} Do you wanto continue this prosess ${green}yes${reset} or ${red}no${reset}? :" answer
+if [ $answer == "yes" ]
+then
+	functiondownload
+	echo "${green} RPD Remote Controle Desktop installed ${reset}"
+else
+	echo "${red} Bye :)) ${reset}"
+fi
